da2a5be7c9dd59c8d75e42043519f43b4eb20d24
wangleihit <you@example.com>
2025-07-21 19:23:29 +0800

    Branch1 print

--- gitTest/main.cpp
+++ gitTest/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "git welcome!" << std::endl;
+
+    std::cout << "This is branch1!" << std::endl;
     return 0;
 }
