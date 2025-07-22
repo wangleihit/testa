2eeb88b3f596c91b2c684d7d61037301bdb813f5
wangleihit <you@example.com>
2025-07-22 18:46:44 +0800

    master主版本合并 增加PrintMessage函数

--- gitTest/main.cpp
+++ gitTest/main.cpp
@@ -3,10 +3,14 @@
 void printMessage(const std::string& msg) {
     std::cout << msg << std::endl;
 }
+void Testabb(const std::string& msg) {
+    std::cout << msg << std::endl;
+}
 int main() {
     add b;
     b.Ptest();
     std::cout << "This is branch2mergeMaster!" << std::endl;
     printMessage("Hello from a new branch0722!");
+    Testabb("Hello abb!");
     return 0;
 }
