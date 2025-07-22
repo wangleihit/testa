7909518c34e506462e9d6daea3d5a87489f15f2d
wangleihit <you@example.com>
2025-07-22 18:44:10 +0800

    master0722 增加PrintMessage函数

--- gitTest/main.cpp
+++ gitTest/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "add.h"
+void printMessage(const std::string& msg) {
+    std::cout << msg << std::endl;
+}
 int main() {
     add b;
     b.Ptest();
     std::cout << "This is branch2mergeMaster!" << std::endl;
+    printMessage("Hello from a new branch0722!");
     return 0;
 }
